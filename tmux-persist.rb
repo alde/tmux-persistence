f073a009d6d065c47181a7dbc0b64ab0d614a4bb
Rickard Dybeck <r.dybeck@gmail.com>
2013-12-12 08:48:20 +0100

    Use snake_case instead of camelCase
    Required for the tmux format commands to work for me

--- tmux-persist.rb
+++ tmux-persist.rb
@@ -1,75 +1,74 @@
 #!/usr/bin/env ruby
 
 # Start - Configuration Variables
-sessionDir = "#{Dir.home}/.sessions"
-maxStoredSessions = 5
-filesToRoll = 3
+session_dir = "#{Dir.home}/.sessions"
+max_stored_sessions = 5
+files_to_roll = 3
 # End - Configuration Variables
 
-Dir::mkdir(sessionDir) unless File.exists?(sessionDir)
+Dir::mkdir(session_dir) unless File.exists?(session_dir)
 
 files = []
-Dir.entries(sessionDir).each do |e|
+Dir.entries(session_dir).each do |e|
   if e !~ /^\./
     files << e
   end
 end
 files.sort!
 
-if files.length > maxStoredSessions
-  0.upto(filesToRoll - 1) do |index|
-    File.delete( sessionDir+ "/" + files[index] )
+if files.length > max_stored_sessions
+  0.upto(files_to_roll - 1) do |index|
+    File.delete( session_dir+ "/" + files[index] )
   end
   puts "Rotated stored sessions"
 end
 
-#%x[rm #{sessionDir}/*-restore]
+#%x[rm #{session_dir}/*-restore]
 
-sessions = %x[tmux list-sessions -F "\#{sessionName}"].split("\n")
+sessions = %x[tmux list-sessions -F "\#{session_name}"].split("\n")
 
-sessions.each do |sessionName|
-  rawPaneList = %x[tmux list-panes -t #{sessionName} -s -F "\#{windowIndex} \#{paneIndex} \#{windowWidth} \#{windowHeight} \#{paneWidth} \#{paneHeight} \#{window_name} \#{pane_current_path} \#{pane_pid}"].split("\n")
-
+sessions.each do |session_name|
+  rawPaneList = %x[tmux list-panes -t #{session_name} -s -F "\#{window_index} \#{pane_index} \#{window_width} \#{window_height} \#{pane_width} \#{pane_height} \#{window_name} \#{pane_current_path} \#{pane_pid}"].split("\n")
   panes = []
   rawPaneList.each do |pane_line|
     temp_pane = pane_line.split(" ")
-    panes.push({
-      windowIndex: Integer(temp_pane[0]),
-      paneIndex: Integer(temp_pane[1]),
-      windowWidth: Integer(temp_pane[2]),
-      windowHeight: Integer(temp_pane[3]),
-      paneWidth: Integer(temp_pane[4]),
-      paneHeight: Integer(temp_pane[5]),
+    panes << {
+      window_index: Integer(temp_pane[0]),
+      pane_index: Integer(temp_pane[1]),
+      window_width: Integer(temp_pane[2]),
+      window_height: Integer(temp_pane[3]),
+      pane_width: Integer(temp_pane[4]),
+      pane_height: Integer(temp_pane[5]),
       window_name: temp_pane[6],
       cwd: temp_pane[7],
       pid: temp_pane[8]
-    })
+    }
   end
 
-  sessionScript = ""
+  session_script = ""
   panes.each do |pane|
     pane[:cmd] = %x[ps --no-headers -o cmd --ppid #{pane[:pid]}].delete("\n")
     pane[:cmd] = %x[ps --no-headers -o cmd #{pane[:pid]}].delete("\n").gsub(/^-/,"") unless pane[:cmd] != ""
 
-    sessionScript += "tmux new-window -t $SESSION -a -n #{pane[:window_name]} \"cd #{pane[:cwd]} && #{pane[:cmd]}\"\n"
+    session_script << "tmux new-window -t $SESSION -a -n #{pane[:window_name]} \"cd #{pane[:cwd]} && #{pane[:cmd]}\"\n"
 
-    if pane[:paneIndex] > 0
-      if pane[:paneWidth] < pane[:windowWidth]
-        sessionScript += "tmux join-pane -h -l #{pane[:paneWidth]} -s $SESSION:#{pane[:windowIndex] +1}.0 -t $SESSION:#{pane[:windowIndex]}\n"
+    if pane[:pane_index] > 0
+      if pane[:pane_width] < pane[:window_width]
+        session_script << "tmux join-pane -h -l #{pane[:pane_width]} -s $SESSION:#{pane[:window_index] +1}.0 -t $SESSION:#{pane[:window_index]}\n"
       else
-        sessionScript += "tmux join-pane -v -l #{pane[:paneHeight]} -s $SESSION:#{pane[:windowIndex] +1}.0 -t $SESSION:#{pane[:windowIndex]}\n"
+        session_script << "tmux join-pane -v -l #{pane[:pane_height]} -s $SESSION:#{pane[:window_index] +1}.0 -t $SESSION:#{pane[:window_index]}\n"
       end
     end
   end
 
-  File.open("#{sessionDir}/#{sessionName}-restore","w") {|f| f.write(%Q[
+  File.open("#{session_dir}/#{session_name}-restore","w") {|f| f.write(%Q[
     #!/usr/bin/env bash
-    SESSION=#{sessionName}
+    SESSION=#{session_name}
 
     if [ -z $TMUX ]; then
 
       # if session already exists, attach
-      tmux has-session -t $SESSION 
+      tmux has-session -t $SESSION
       if [ $? -eq 0 ]; then
         echo \"Session $SESSION already exists. Attaching...\"
         tmux attach -t $SESSION
@@ -79,7 +78,7 @@ sessions.each do |sessionName|
       # make new session
       tmux new-session -d -s $SESSION
 
-    #{sessionScript}
+    #{session_script}
 
       # attach to new session
       tmux select-window -t $SESSION:1
