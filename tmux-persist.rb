955e049890d9731b7740b765f2d83391678f5893
Rickard Dybeck <r.dybeck@gmail.com>
2013-12-12 08:47:16 +0100

    Use Dir.home instead of ~

--- tmux-persist.rb
+++ tmux-persist.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # Start - Configuration Variables
-sessionDir = "~/.sessions"
+sessionDir = "#{Dir.home}/.sessions"
 maxStoredSessions = 5
 filesToRoll = 3
 # End - Configuration Variables
